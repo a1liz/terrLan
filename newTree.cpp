4c34bac89cb2b8ec8e6350ccba32696867848035
a1liz <694426737@qq.com>
2018-01-03 22:04:59 +0800

    update syntax tree

--- newTree.cpp
+++ newTree.cpp
@@ -10,67 +10,217 @@ extern symbol_table symbtl;
 
 Tree parse_tree;
 
-int tree::node_seq = 0;
-int tree::temp_var_seq = 0;
-int tree::label_seq = 0;
+int Tree::node_seq = 0;
+int Tree::temp_var_seq = 0;
+int Tree::label_seq = 0;
 
 void Node::output(void) {
     
 }
 
-void Tree::type_check(Node* t) {
+Node* Tree::NewRoot(int nodetype, int subtype, int valuetype, NodeAttr attr, Node* child1, Node* child2, Node* child3, Node* child4) {
+    Node *t = new Node();
+    if (NULL == t)
+        cerr << "Out memory at line %d\n" << lineno;
+    else {
+        t->nodetype = nodetype;
+        t->subtype = subtype;
+        t->valuetype = valuetype;
+        t->children.push_back(child1);
+        t->children.push_back(child2);
+        t->children.push_back(child3);
+        t->children.push_back(child4);
+        t->lineno = lineno;
+        t->seq = Tree::node_seq++;
+        root = t;
+        type_check(t);
+        get_temp_var(t);
+    }
+    return t;
+}
 
+void Tree::type_check(Node *t) {
+    if (t->valuetype != Notype)
+        return;
+    if (t->nodetype == STMT_NODE) {
+        if (t->type_check == IF_STMT || t->subtype == WHILE_STMT)
+            if (t->children[0]->type != Boolean) {
+                cerr << "Bad boolean type at line: " << t->lineno << endl;
+                exit(1);
+            } else
+                return;
+    }
+    if (t->nodetype != EXPR_NODE)
+        return;
+    if (t->subtype != BINARY_EXPR && t->subtype != UNARY_EXPR)
+        return;
+    switch(t->subtype) {
+        case BINARY_EXPR: {
+            Node *arg1 = t->children[0];
+            Node *arg2 = t->children[1];
+            switch(t->attr.op) {
+                case RIGHT_OP:
+                case LEFT_OP: {
+                    if(arg1->valuetype != Integer || arg2->valuetype != Integer) {
+                        cerr << "Bad type at line: " << t->lineno << endl;
+                        exit(1);
+                    }
+                    t->valuetype = arg1->valuetype;
+                    break;
+                }
+                case AND_OP:
+                case OR_OP: {
+                    if (arg1->valuetype != Boolean || arg2->valuetype != Boolean) {
+                        cerr << "Bad type at line: " << t->lineno << endl;
+                        exit(1);
+                    }
+                    t->valuetype = arg1->valuetype;
+                    break;
+                }
+                case LE_OP:
+                case GE_OP:
+                case EQ_OP:
+                case NE_OP: {
+                    if (arg1->valuetype != arg2->valuetype) {
+                        cerr << "Bad type at line: " << t->lineno << endl;
+                        exit(1);
+                    }
+                    t->valuetype = Boolean;
+                    break;
+                }
+                case AND:
+                case XOR:
+                case OR:
+                case ADD:
+                case SUB:
+                case MUL:
+                case DIV:
+                case MOD: {
+                    if (arg1->valuetype != Integer || arg2->valuetype != Integer) {
+                        cerr << "Bad type at line: " << t->lineno << endl;
+                        exit(1);
+                    }
+                    t->valuetype = arg1->valuetype;
+                    break;
+                }
+                default: {
+                    cerr << "Unknown binary operation at line: " << t->lineno << endl;
+                    exit(1);
+                }
+            }
+        }
+        case UNARY_EXPR: {
+            Node *arg1 = t->children[0];
+            switch(t->attr.op) {
+                case INC_OP:
+                case DEC_OP:
+                case NOT:
+                case TLIDE:
+                case SUB: {
+                    if (arg1->valuetype != Integer) {
+                        cerr << "Bad type at line: " << t->lineno << endl;
+                        exit(1);
+                    }
+                    t->valuetype = arg1.valuetype;
+                    break;
+                }
+                default: {
+                    cerr << "Unknown unary operation at line: " << t->lineno << endl;
+                    exit(1);
+                }
+            }
+        }
+        default:
+            return;
+    }
+    return;
 }
 
 void Tree::get_temp_var(Node* t) {
-
-}
-
-char* Tree::new_label() {
-
-}
-
-void Tree::recursive_get_label(Node *t) {
-
-}
-
-void Tree::stmt_get_label(Node *t) {
-
-}
-
-void Tree::expr_get_label(Node *t) {
-
-}
-
-void Tree::gen_header(ostream &out) {
-
-}
-
-void Tree::gen_decl(ostream &out, Node *t) {
-
-}
-
-void Tree::recursive_gen_code(ostream &out, Node *t) {
-
-}
-
-void Tree::stmt_gen_code(ostream &out, Node *t) {
-
-}
-
-void Tree::expr_gen_code(ostream &out, Node *t) {
-
-}
-
-
-Node* Tree::NewRoot(int kind, int kind_kind, NodeAttr attr, int type, vector<Node*> children) {
-
-}
-
-void Tree::get_label() {
-
-}
-
-void gen_code(ostream &out) {
+    if (t->nodetype != EXPR_NODE)
+        return;
     
+    Node *arg1 = t->children[0];
+    Node *arg2 = t->children[1];
+
+    if (arg1->subtype == OP_EXPR)
+        temp_var_seq--;
+    if (arg2 && arg2->subtype == OP_EXPR)
+        temp_var_seq--;
+    t->temp_var = Tree::temp_var_seq;
+    Tree::temp_var_seq++;
 }
+
+// char* Tree::new_label(void) {
+//     char tmp[20];
+
+//     sprintf(tmp, "@%d", Tree::label_seq);
+//     Tree::label_seq++;
+//     return tmp;
+// }
+
+// void Tree::stmt_get_label(Node *t) {
+//     switch(t->subtype) {
+//         case COMP_STMT: {
+
+//         }
+//         case IF_STMT: {
+
+//         }
+//         case WHILE_STMT: {
+
+//         }
+//     }
+// }
+
+// void Tree::expr_get_label(Node *t) {
+//     if (t->valuetype != Boolean)
+//         return;
+//     Node *e1 = t->children[0];
+//     Node *e2 = t->children[1];
+//     switch(t->attr.op) {
+//         case AND: {
+
+//         }
+//         case OR: {
+
+//         }
+//         case NOT: {
+
+//         }
+
+//         if (e1)
+//             recursive_get_label(e1);
+//         if (e2)
+//             recursive_get_label(e2);
+//     }
+// }
+
+// void Tree::recursive_get_label(Node *t) {
+//     if (t->nodetype == STMT_NODE)
+//         stmt_get_label(t);
+//     else if (t->nodetype == EXPR_NODE)
+//         expr_get_label(t);
+    
+// }
+
+// void Tree::get_label(void) {
+//     Node *p = root;
+
+//     p->label.begin_label = "_start";
+//     recursive_get_label(p);
+// }
+
+// void Tree::gen_header(ostream &out) {
+//     out << "\t.586" << endl;
+//     out << "\t.model flat, stdcall" << endl;
+//     out << "\toption casemap :none" << endl;
+//     out << endl;
+    
+// }
+
+// void Tree::gen_decl(ostream &out, Node *t) {
+//     out << endl << endl << "\t.data" << endl;
+
+//     for(; t->nodetype == DECL_NODE; t = )
+// }
--- newTree.h
+++ newTree.h
@@ -7,17 +7,90 @@ using namespace std;
 #ifndef TREE_H
 #define TREE_H
 
+#define MAX_CHILDREN 4
+
+enum {
+    PROGRAM = 0,TRANSLATION_UNIT,EXTERNAL_DECLARATION,FUNCTION_DEFINITION,DECLARATION_PREFIX,SPECIAL_SPECIFIER,
+    TYPE_DEFINE,DECLARATION,INIT_DECLARATION_LIST,INIT_DECLARATION,DECLARATION_ITEM,
+    SIMPLE_ITEM,IDENTIFIER_LIST,INITIALIZER,INITIALIZER_LIST,ASSIGNMENT_EXPR,CONDITION_EXPR,LOGICAL_EXPR,BIT_EXPR,
+    EQUALITY_EXPR,RELATIONAL_EXPR,SHIFT_EXPR,ADD_EXPR,MUL_EXPR,CAST_EXPR,UNARY_EXPR,UNARY_OP,
+    POSTFIX_EXPR,ARGUMENT_EXPR_LIST,PRIMARY_EXPR,IDENTIFIER,CONSTANT_EXPR,STRING_CONSTANT,PARAMETER_LIST,PARAMETER_DECLARATION,
+    COMPOUND_STATEMENT,COMPOUND_STATEMENT_LIST,DECLARATION_LIST,STATEMENT_LIST,STATEMENT,LABELED_STATEMENT,
+    EXPR_STATEMENT,CONDITION_STATEMENT,EXPR,LOOP_STATEMENT,JUMP_STATEMENT,OPERATOR,KEYWORD,NONE
+};
+
+enum {
+    STMT_NODE = 0,
+    EXPR_NODE,
+    DECL_NODE
+};
+
+enum {
+    IF_STMT = 0,
+    WHILE_STMT,
+    INPUT_STMT,
+    PRINT_STMT,
+    COMP_STMT
+};
+
+enum {
+    TYPE_EXPR = 0,
+    BINARY_EXPR,
+    UNARY_EXPR,
+    ARRAY_EXPR,
+    CONST_EXPR,
+    ID_EXPR
+};
+
+enum {
+    VAR_DECL = 0,
+    ARRAY_DECL
+};
+
+enum {
+    Notype = 0,
+    Integer,
+    Boolean,
+    Character
+};
+
+// enum {
+//     RIGHT_OP = 0,
+//     LEFT_OP,
+//     INC_OP,
+//     DEC_OP,
+//     AND_OP,
+//     OR_OP,
+//     LE_OP,
+//     GE_OP,
+//     EQ_OP,
+//     NE_OP,
+//     AND,
+//     XOR,
+//     OR,
+//     ADD,
+//     SUB,
+//     MUL,
+//     DIV,
+//     MOD,
+//     NOT,
+//     TLIDE,
+//     PTR_OP
+// };
 
 union NodeAttr {
     int op;
     int vali;
     char valc;
+    bool valb;
+    
     int symbl_seq;
 
     NodeAttr(void) {op = 0;}
-    NodeAttr(int i) {op = i;}
-    NodeAttr(char c) {valc = c};
-}
+    NodeAttr(int i) {vali = i;}
+    NodeAttr(char c) {valc = c;}
+    NodeAttr(bool b) {valb = b;}
+};
 
 union Label {
     struct {
@@ -28,18 +101,17 @@ union Label {
         char* begin_label;
         char* next_label;
     };
-}
+};
 
 class Node {
 public:
     vector<Node*> children;
-    vector<Node*> sibling;
     int lineno;
-    int kind;
-    int kind_kind;
-    NodeAttr attr;
-    int type;
-    int seq;
+    int nodetype; // 节点种类
+    int subtype; // 节点子种类
+    int valuetype; // 结点属性的种类
+    NodeAttr attr; // 结点属性的具体值
+    int seq; // 节点在语法树中的序号
     int temp_var;
     Label label;
 
@@ -49,24 +121,26 @@ public:
 class Tree {
 private:
     Node *root;
-    static node_seq;
-    static temp_var_seq;
-    static label_seq;
+    static int node_seq; 
+    static int temp_var_seq;
+    static int label_seq;
 
+    
     void type_check(Node *t);
     void get_temp_var(Node *t);
-    char* new_label(void);
-    void recursive_get_label(Node *t);
-    void stmt_get_label(Node *t);
-    void expr_get_label(Node *t);
-    void gen_header(ostream &out);
-    void gen_decl(ostream &out, Node *t);
-    void recursive_gen_code(ostream &out, Node *t);
-    void stmt_gen_code(ostream &out, Node *t);
-    void expr_gen_code(ostream &out, Node *t);
+    
+    // char* new_label(void);
+    // void recursive_get_label(Node *t);
+    // void stmt_get_label(Node *t);
+    // void expr_get_label(Node *t);
+    // void gen_header(ostream &out);
+    // void gen_decl(ostream &out, Node *t);
+    // void recursive_gen_code(ostream &out, Node *t);
+    // void stmt_gen_code(ostream &out, Node *t);
+    // void expr_gen_code(ostream &out, Node *t);
 
 public:
-    Node* NewRoot(int kind, int kind_kind, NodeAttr attr, int type, vector<Node*> children);
+    Node* NewRoot(int nodetype, int type_type, int valuetype, NodeAttr attr, Node* child1 = NULL, Node* child2 = NULL, Node* child3 = NULL, Node* child4 = NULL);
     void get_label(void);
     void gen_code(ostream &out);
 };
